1efa2a45d084a1e09b9ddf3e03cb1f83ea104686
armaBurton <armanarma@armaburton.com>
2022-03-23 16:57:13 -0700

    don't use this

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,4 +11,45 @@ describe('backend-bookstore routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('returns an array of publishers', async () => {
+    // const expected = await Publisher.getAllPublishers();
+    const res = await request(app).get('/api/v1/publishers');
+
+    expect(res.body).toEqual(
+      expect.arrayContaining([expect.objectContaining({})])
+    );
+  });
+
+  it('returns an array of books', async () => {
+    const res = await request(app).get('/api/v1/books');
+
+    expect(res.body).toEqual(
+      expect.arrayContaining([expect.objectContaining({})])
+    );
+  });
+
+  it('returns an array of authors', async () => {
+    const res = await request(app).get('/api/v1/authors');
+
+    expect(res.body).toEqual(
+      expect.arrayContaining([expect.objectContaining({})])
+    );
+  });
+
+  it('returns an array of reviewers', async () => {
+    const res = await request(app).get('/api/v1/reviewers');
+
+    expect(res.body).toEqual(
+      expect.arrayContaining([expect.objectContaining({})])
+    );
+  });
+
+  it('returns an array of top 100 reviews', async () => {
+    const res = await request(app).get('/api/v1/reviews');
+    expect(res.body).toEqual(
+      expect.arrayContaining([expect.objectContaining({})])
+    );
+  });
+
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,17 +1,90 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS publisher;
+DROP TABLE IF EXISTS publishers CASCADE;
+DROP TABLE IF EXISTS authors CASCADE;
+DROP TABLE IF EXISTS books CASCADE;
+DROP TABLE IF EXISTS reviews CASCADE;
+DROP TABLE IF EXISTS reviewers CASCADE;
+DROP TABLE IF EXISTS author_book CASCADE;
 
 
-CREATE TABLE publisher (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+CREATE TABLE publishers (
+    publisher_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT NOT NULL,
-    city TEXT NOT NULL,
-    country TEXT NOT NULL
+    city TEXT,
+    state TEXT,
+    country TEXT
 );
 
 INSERT INTO 
-publisher (name, city, country)
+publishers (name, city, state, country)
 VALUES
-('Tor Books', 'New York City', 'USA'),
-('Penguin Random House Company', 'New York City', 'USA');
+('Tor Books', 'New York City', 'New York', 'USA'),
+('Penguin Random House Company', 'New York City', 'New York', 'USA');
+
+CREATE TABLE authors (
+    author_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    date_of_birth DATE,
+    place_of_birth TEXT
+);
+
+INSERT INTO
+authors (name, date_of_birth, place_of_birth)
+VALUES
+('John Steinback', '1953-11-12', 'Salinas, CA' ),
+('Neal Town Stephenson', '1959-10-31', 'Fort Meade, MD');
+
+
+CREATE TABLE books (
+    book_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    publisher_id BIGINT NOT NULL,
+    review_id BIGINT,
+    title TEXT NOT NULL,
+    released INT NOT NULL
+);
+
+INSERT INTO
+books (title, released, publisher_id, review_id)
+VALUES
+('Mice and Men', 1937, 1, 1),
+('The Diamond Age: Or, A Young Lady''s Illustrated Primer',  1995, 2, 2);
+
+
+CREATE TABLE reviews (
+    review_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    reviewer_id BIGINT NOT NULL,
+    review_text CHAR(140) NOT NULL,
+    rating INT NOT NULL,
+    book_id BIGINT NOT NULL
+);
+
+INSERT INTO
+reviews (reviewer_id, review_text, rating, book_id)
+VALUES
+(1, 'Whether it is Snapchat, Twitter, Facebook, Yelp or just a note to co-workers or business officials, the number of actual characters matters.', 5, 1),
+(2, 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma', 4, 2);
+
+CREATE TABLE reviewers (
+    reviewer_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    company TEXT NOT NULL
+
+);
+
+INSERT INTO
+reviewers (name, company)
+VALUES
+('Yon Yonson', 'Ratty Comic Books'),
+('Betsy Bonsei', 'TV Guide Dinner Reviews');
+
+
+CREATE TABLE author_book (
+    book_id BIGINT REFERENCES books(book_id),
+    author_id BIGINT REFERENCES authors(author_id)
+);
+
+-- INSERT INTO
+-- author_book (book_id, author_id)
+-- VALUES
+-- ()
